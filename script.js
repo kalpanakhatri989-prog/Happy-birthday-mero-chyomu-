515fbf76bf95392a8bd29055316295b0d20d8e21
kalpanakhatri989-prog <kalpanakhatri989@gmail.com>
2026-07-28 14:20:01 +0545

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,24 @@ openBtn.addEventListener("click", () => {
     type();
 
 });
+function createPetal(){
+
+    const petal=document.createElement("div");
+
+    petal.innerHTML="🌸";
+
+    petal.classList.add("petal");
+
+    petal.style.left=Math.random()*100+"vw";
+
+    petal.style.animationDuration=(5+Math.random()*5)+"s";
+
+    document.body.appendChild(petal);
+
+    setTimeout(()=>{
+        petal.remove();
+    },10000);
+
+}
+
+setInterval(createPetal,500);

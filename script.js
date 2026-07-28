1bdb41eb218805d2c81f0346d11b4a4c6d941594
kalpanakhatri989-prog <kalpanakhatri989@gmail.com>
2026-07-28 14:09:13 +0545

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,28 @@ const hero = document.getElementById("hero");
 const message = document.getElementById("message");
 
 btn.addEventListener("click", () => {
+
     hero.style.display = "none";
     message.style.display = "block";
+
+    setInterval(createHeart,300);
+
 });
+
+function createHeart(){
+
+    const heart=document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML="❤️";
+
+    heart.style.left=Math.random()*100+"vw";
+
+    document.body.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },6000);
+
+}
